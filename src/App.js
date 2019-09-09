35d0f60a47082ff32ec41a397e2596cd0f4d8779
farayolaj <farayolajoshua@gmail.com>
2019-09-09 12:45:34 +0100

    Homepage added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import Popup from 'reactjs-popup';
+import SignaturePad from 'react-signature-canvas';
 import './App.css';
+import './sigCanvas.css';
 
 function App() {
+  const [ imgURL, setImgURL ] = useState(null);
+  const sigCanvas = useRef({ });
+  const clear = () => sigCanvas.current.clear();
+  const save = () => {
+    const trim = sigCanvas.current.getTrimmedCanvas().toDataURL("image/png");
+    setImgURL(trim);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Signature Pad Example</h1>
+      <Popup modal trigger={<button>Open Signature Pad</button>} closeOnDocumentClick={false}>
+        {close => (
+          <>
+            <SignaturePad ref={sigCanvas} canvasProps={{
+              className: "signatureCanvas",
+            }}/>
+            <button onClick={close}>Close</button>
+            <button onClick={clear}>Clear</button>
+            <button onClick={save}>Save</button>
+          </>
+        )}
+      </Popup>
+      <br />
+      <br />
+      {imgURL ? <img src={imgURL} alt="alt" style={{
+        display: "block",
+        margin: "0 auto",
+        border: "1px solid black",
+        width : "150px"
+      }} /> : null}
     </div>
   );
 }
